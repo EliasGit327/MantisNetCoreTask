de79e047f7c44ade697f1f192721045161ae6a23
elias <Zombie9876>
2020-09-17 20:14:38 +0300

    Initial commit

---
+++ MantisNetCoreTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MantisNetCoreTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
